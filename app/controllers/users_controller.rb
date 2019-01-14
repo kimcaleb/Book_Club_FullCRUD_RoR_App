5c4e5123f621610fa0e19909b6b78e8e6372c642
Caleb Kim <calebkim2005@yahoo.com>
2019-01-14 15:42:38 -0800

    Completed create and new methods for User COntroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,41 @@
 class UsersController < ApplicationController
   
   def index
+
   end
 
   def show
+
   end
 
   def new
+    @user = user.new #so we have the empty user object link to the form where we start mapping the different fields to the different attributes/columns
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save # returns truthy or falsey based on whether it was saved sucessfully
+      redirect_to new_session_path #
+    end
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def destroy
+
   end
+
+  private
+  def user_params #remember the params object is coming from the form via the http request that was intercepted by the route which was routed to this controller. The permit method is basically saying grab these attributes out of the form by their name
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  
   root "users#index"
   resources :users do # automatically generates all restful routes for a particular controller. 
     resources :books
   end
-
-
-
 end
