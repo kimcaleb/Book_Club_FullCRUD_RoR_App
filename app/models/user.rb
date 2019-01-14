258453df1cf2a5596ce07af4f085ad5574f4a01c
Caleb Kim <calebkim2005@yahoo.com>
2019-01-14 14:14:25 -0800

    Created User Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
- #invokes b-crypt to require and hash passwords when adding new users. 
+ #invokes b-crypt to require and hash passwords when adding new users. Provides many methods for the user object such as @user.authenticate()
     has_secure_password
 end
